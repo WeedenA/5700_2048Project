9ac7026500911cce9c0478fa312039588b587aba
Tsuits <ajw0057@auburn.edu>
2021-03-12 15:56:20 -0600

    Green Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -1,4 +1,7 @@
 def _create(userParms):
     result = dict.fromkeys(['grid', 'score', 'Integrity', 'status'])
-    
+    result['grid'] = '0'
+    result['score'] = 0
+    result['Integrity'] = 'somehashthing'
+    result['status'] = 'pass/fail' 
     return result
