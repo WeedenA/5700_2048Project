b2295ad9b1df5f75892de08c4752d830386d71a9
Tsuits <ajw0057@auburn.edu>
2021-03-12 14:56:18 -0600

    Green Light |

--- Tiles2048/test/createTest.py
+++ Tiles2048/test/createTest.py
@@ -6,6 +6,12 @@ class CreateTest(unittest.TestCase):
 #  The following is included only for illustrative purposes.  It tests nothing
 #  useful.  Please delete and replace with your own test code.
     def test_create_HappyPathTest010(self):
-        userParms = {'op': 'create', 'size': '4'}
+        userParms = {'op': 'create'}
         actualResult = create._create(userParms)
         self.assertIsNotNone(actualResult)
+    def test_create_HappyPathStringUnderGrid(self):
+        expectedResult = {'create': 'create stub'}
+        userParms = {'op': 'create'}
+        actualResult = create._create(userParms)
+        self.assertDictEqual(expectedResult, actualResult)
+    
