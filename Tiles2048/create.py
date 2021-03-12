478cd14be50060ac11103b407193c0775d7030b7
Tsuits <ajw0057@auburn.edu>
2021-03-12 16:46:32 -0600

    Refactor |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -2,17 +2,19 @@ import random
 import hashlib
 def _create(userParms):
     result = dict.fromkeys(['grid', 'score', 'Integrity', 'status'])
-    result['grid'] = '0'
+    
     gridWidth = 4 # predetermined by game ruleset
     gridHeight = 4 # predetermined by game ruleset
     totalGridTiles = gridWidth * gridHeight
     gameGridList = [0] * totalGridTiles
+    
     firstRandomTile = random.randint(0,15)
     secondRandomTile = random.randint(0,15) 
     while (firstRandomTile == secondRandomTile):
         secondRandomTile = random.randint(0,15)
     gameGridList[firstRandomTile] = 2
-    gameGridList[secondRandomTile] = 2     
+    gameGridList[secondRandomTile] = 2   
+      
     gameGridString = ''
     for value in gameGridList:
         gameGridString += str(value)
