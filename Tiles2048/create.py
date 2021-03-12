266da73463f03da509b612c6c036f1821f116e28
Tsuits <ajw0057@auburn.edu>
2021-03-12 16:17:21 -0600

    Red Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -1,14 +1,21 @@
+import random.randint
 def _create(userParms):
     result = dict.fromkeys(['grid', 'score', 'Integrity', 'status'])
     result['grid'] = '0'
     gridWidth = 4 # predetermined by game ruleset
     gridHeight = 4 # predetermined by game ruleset
     totalGridTiles = gridWidth * gridHeight
-    startingGridList = [0] * totalGridTiles
-    startingGridString = ''
-    for value in startingGridList:
-        startingGridString += str(value)
-    result['grid'] = startingGridString
+    gameGridList = [0] * totalGridTiles
+        
+    gameGridString = ''
+    for value in gameGridList:
+        gameGridString += str(value)
+    result['grid'] = gameGridString
+    
+    
+    
+    
+    
     result['score'] = 0
     result['Integrity'] = 'somehashthing'
     result['status'] = 'pass/fail' 
