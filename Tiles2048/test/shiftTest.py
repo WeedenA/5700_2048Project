9d85a894603cb0d8ea62f8c6e359fda99163266f
Tsuits <ajw0057@auburn.edu>
2021-03-19 16:13:18 -0500

    Green Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -10,6 +10,16 @@ from pickle import FALSE
 
 class CreateTest(unittest.TestCase):
     def test_shift_010HappyStartingGridNoCombinesDown(self):
+        substring = '22224444'
+        userParms = {'op': 'shift', 'grid': '2222000044440000', 'direction': 'down',
+                     'score': '0', 'integrity': 'ASDHASHD'}
+        actualResult = shift._shift(userParms)['grid']
+        if substring not in actualResult:
+            isValidGrid = False
+        else: 
+            isValidGrid = True
+        self.assertTrue(isValidGrid)
+    def test_shift_010HappyRandomGridCombinesDown(self):
         substring = '44448888'
         userParms = {'op': 'shift', 'grid': '2222222244444444', 'direction': 'down',
                      'score': '0', 'integrity': 'ASDHASHD'}
@@ -19,4 +29,3 @@ class CreateTest(unittest.TestCase):
         else: 
             isValidGrid = True
         self.assertTrue(isValidGrid)
-    
