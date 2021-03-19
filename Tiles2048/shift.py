40bfc1c5929fd065c9ba4e419411ee2d5b56fa09
Tsuits <ajw0057@auburn.edu>
2021-03-19 15:28:45 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -1,3 +1,37 @@
 def _shift(userParms):
-    result = userParms
-    return result
+    result = dict.fromkeys(['grid', 'score', 'Integrity', 'status'])
+    
+    gameGridList = handleInputGrid(userParms)
+    
+    mergedGrid = merge(gameGridList)
+    
+    
+#     
+#     inputGrid = userParms['grid']
+#     gameGridList = [0] * 16
+#     for i in range(len(inputGrid)):
+#         gameGridList[i] = inputGrid[i]
+    
+    
+    
+    
+    
+    
+    
+    
+    return inputGrid
+
+def handleInputGrid(inputParms):
+    inputGrid = []
+    inputGrid[:0] = inputParms['grid']
+    for i in range(len(inputGrid)):
+        inputGrid[i] = int(inputGrid[i])
+    return inputGrid
+
+def merge(gameGrid):
+    for i in range(15,3,-1):
+        if gameGrid[i] == gameGrid[i-4]:
+            
+
+def merge():
+    return 0
--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -8,7 +8,7 @@ import Tiles2048.shift as shift
 
 
 class CreateTest(unittest.TestCase):
-    def test_shift_010HappyStartingGridNoCombines(self):
+    def test_shift_010HappyStartingGridNoCombinesDown(self):
         expectedResult = {'grid': '0000000000002200', 'score': '0', 'integrity': 'ASDHASHD', 
                           'status': 'ok'}
         userParms = {'op': 'shift', 'grid': '0000200002000000', 'direction': 'down',
