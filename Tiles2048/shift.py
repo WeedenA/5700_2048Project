050305f6d1d6e0b263cc5faaf6c4a53b43bf013c
Tsuits <ajw0057@auburn.edu>
2021-03-19 16:05:50 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -1,32 +1,24 @@
 def _shift(userParms):
-    result = dict.fromkeys(['grid', 'score', 'Integrity', 'status'])
+    result = dict.fromkeys(['grid', 'score', 'integrity', 'status'])
+    result['score'] = userParms['score']
+    result['integrity'] = userParms['integrity']
+    result['status'] = 'ok'
+    
+    
     
     gameGridList = handleInputGrid(userParms)
-    
+    print(gameGridList)
     mergedGrid = merge(gameGridList)
+    print(mergedGrid)
     collapsedGrid = collapse(mergedGrid)
-    
-    
-#     
-#     inputGrid = userParms['grid']
-#     gameGridList = [0] * 16
-#     for i in range(len(inputGrid)):
-#         gameGridList[i] = inputGrid[i]
-    
-    
-    
-    
-    
-    
-    
-    
-    return collapsedGrid
+    print(collapsedGrid)
+    finalGridString = ''.join(map(str, collapsedGrid))  
+    result['grid'] = finalGridString
+    return result
 
 def handleInputGrid(inputParms):
-    inputGrid = []
-    inputGrid[:0] = inputParms['grid']
-    for i in range(len(inputGrid)):
-        inputGrid[i] = int(inputGrid[i])
+    inputGrid = [int(i) for i in inputParms['grid']]
+    print(inputGrid)
     return inputGrid
 
 def merge(gameGrid):
@@ -36,9 +28,14 @@ def merge(gameGrid):
             gameGrid[i-4] = 0
     return gameGrid 
 
-def collapse(gameGrid):
-    for i in range(15,3,-1):
-        if gameGrid[i] == 0 and gameGrid[i-4] != 0:
-            gameGrid[i] = gameGrid[i-4]
-            gameGrid[i-4] = gameGrid
+def collapse(gameGrid):  
+    isDone = False
+    while(not isDone):
+        isDone = True
+        for i in range(15,3,-1):
+            tileAbove = i-4
+            if gameGrid[i] == 0 and gameGrid[tileAbove] != 0:
+                gameGrid[i] = gameGrid[tileAbove]
+                gameGrid[tileAbove] = gameGrid
+                isDone = False
     return gameGrid
--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -9,10 +9,10 @@ import Tiles2048.shift as shift
 
 class CreateTest(unittest.TestCase):
     def test_shift_010HappyStartingGridNoCombinesDown(self):
-        expectedResult = {'grid': '0000000000002200', 'score': '0', 'integrity': 'ASDHASHD', 
-                          'status': 'ok'}
-        userParms = {'op': 'shift', 'grid': '2222222200000000', 'direction': 'down',
+        expectedResult = '0000000044448888'
+        userParms = {'op': 'shift', 'grid': '2222222244444444', 'direction': 'down',
                      'score': '0', 'integrity': 'ASDHASHD'}
         actualResult = shift._shift(userParms)
+        actualResult = actualResult['grid']
         self.assertEqual(expectedResult, actualResult)
     
