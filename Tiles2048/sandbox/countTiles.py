184b4e0f21c81dfadfb465d726e73e9072068b0e
Tsuits <ajw0057@auburn.edu>
2021-03-19 15:32:46 -0500

    Red Light |

--- Tiles2048/sandbox/countTiles.py
+++ Tiles2048/sandbox/countTiles.py
@@ -3,10 +3,4 @@ Created on Mar 12, 2021
 
 @author: Alex Weeden
 '''
- startingTiles = 0
-    totalTiles = 0
-    for digit in string:
-        if digit == 2:
-            startingTiles += 1
-        totalTiles += 1
-    assert start ==2 total ==16
+ inputGrid = [2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4]
