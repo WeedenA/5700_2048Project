8e397dd34c669e3daea0a220d469c1b9688223a0
Tsuits <ajw0057@auburn.edu>
2021-04-29 12:35:06 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -109,6 +109,7 @@ def assessRound(grid, gridString):
         for dir in directions:
             if _shift({'op': 'shift', 'grid': gridString, 'direction': dir, 'score': '0', 
                       'integrity': INTEGRITY})['status'] != 'error - no shift possible':
+                return 'ok'
         return 'lose'
     return 'ok'
 
