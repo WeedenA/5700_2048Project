b1a394188a36a8379caf9d79857de7b4a1636947
Tsuits <ajw0057@auburn.edu>
2021-04-29 11:47:19 -0500

    Refactor |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -125,7 +125,7 @@ class CreateTest(unittest.TestCase):
     def test_shift_221SadEmptyScoreGiven(self):
         expectedResult = {'status': 'error - missing score'}
         userParms = {'op': 'shift', 'grid': '0044224422442244', 'direction': 'right',
-                     'score' = '','integrity': INTEGRITY}
+                     'score': '','integrity': INTEGRITY}
         actualResult = shift._shift(userParms)
         self.assertEqual(expectedResult, actualResult)
     def test_shift_230SadNoMovesAvailable(self):
